7e22ebf0d8aa5f1371604b383b5208af2b6f1e3e
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2018-05-19 17:52:47 +0200

    updated watch command for js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,213 +1,212 @@
-"use strict";
+'use strict'
 
 // ==========================================================================
 // Require plugins
 // ==========================================================================
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var maps = require('gulp-sourcemaps');
-var del = require('del');
-var bs = require('browser-sync').create();
-var bower = require('gulp-bower');
-var imagemin = require('gulp-imagemin');
-var newer = require('gulp-newer');
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
+var sass = require('gulp-sass')
+var maps = require('gulp-sourcemaps')
+var del = require('del')
+var bs = require('browser-sync').create()
+var bower = require('gulp-bower')
+var imagemin = require('gulp-imagemin')
+var newer = require('gulp-newer')
+var plumber = require('gulp-plumber')
+var notify = require('gulp-notify')
 
 // Browser Sync
 // ==========================================================================
 gulp.task('browser-sync', function () {
-    bs.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
+  bs.init({
+    server: {
+      baseDir: './'
+    }
+  })
+})
 
 // Custom plumber function to prevent errors
 // Also prevents stopping of running tasks on error
 // ==========================================================================
-function customPlumber(errTitle) {
-    return plumber({
-        errorHandler: notify.onError({
-            title: errTitle || "Error running Gulp",
-            message: "Error <%= error.message %>",
-            sound: "Pop"
-        })
-    });
+function customPlumber (errTitle) {
+  return plumber({
+    errorHandler: notify.onError({
+      title: errTitle || 'Error running Gulp',
+      message: 'Error <%= error.message %>',
+      sound: 'Pop'
+    })
+  })
 }
 
 // Compress images
 // ==========================================================================
 gulp.task('images', function () {
-    return gulp.src('assets/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(newer('dist/images/')) // Only optimize new files
-        .pipe(imagemin({
-            progressive: true,
-            optimazationlevels: 5, // default = 3
-            multipass: true,
-            SVGOPlugins: [
-                {'removeTitle': true},
-                {'removeUselessStrokeAndFill': false}
-            ]
-        }))
-        .pipe(gulp.dest('dist/images/'))
-});
+  return gulp.src('assets/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(newer('dist/assets/images/')) // Only optimize new files
+    .pipe(imagemin({
+      progressive: true,
+      optimazationlevels: 5, // default = 3
+      multipass: true,
+      SVGOPlugins: [
+        { 'removeTitle': true },
+        { 'removeUselessStrokeAndFill': false }
+      ]
+    }))
+    .pipe(gulp.dest('dist/assets/images/'))
+})
 
 // Video
 // ==========================================================================
 gulp.task('video', function () {
-    return gulp.src('assets/video/**/*.+(mp4|webm)')
-        .pipe(newer('dist/video/'))
-        .pipe(gulp.dest('dist/assets/video/'))
-});
+  return gulp.src('assets/video/**/*.+(mp4|webm)')
+    .pipe(newer('dist/video/'))
+    .pipe(gulp.dest('dist/assets/video/'))
+})
 
 // Copy fonts
 // ==========================================================================
 gulp.task('fonts', function () {
-    return gulp.src('assets/fonts/**/*')
-        .pipe(newer('dist/assets/fonts/'))
-        .pipe(gulp.dest('dist/assets/fonts/'))
-});
+  return gulp.src('assets/fonts/**/*')
+    .pipe(newer('dist/assets/fonts/'))
+    .pipe(gulp.dest('dist/assets/fonts/'))
+})
 
 // Merge .js files into app.js
 // ==========================================================================
-gulp.task("concatScripts", function () {
-    return gulp.src([
-        'assets/js/dependencies/*.js',
-        'assets/js/*.js',
-        '!assets/js/app.js'
-    ])
-        .pipe(concat('app.js'))
-        .pipe(newer('assets/js/app.js'))
-        .pipe(gulp.dest('assets/js'));
-});
+gulp.task('concatScripts', function () {
+  return gulp.src([
+    'assets/js/dependencies/*.js',
+    'assets/js/*.js',
+    '!assets/js/app.js'
+  ])
+    .pipe(concat('app.js'))
+    .pipe(newer('assets/js/app.js'))
+    .pipe(gulp.dest('assets/js'))
+})
 
 // Minify app.js
 // ==========================================================================
-gulp.task("minifyScripts", ["concatScripts"], function () {
-    return gulp.src("assets/js/app.js")
-        .pipe(uglify())
-        .pipe(rename('app.min.js'))
-        .pipe(gulp.dest('dist/assets/js'));
-});
+gulp.task('minifyScripts', ['concatScripts'], function () {
+  return gulp.src('assets/js/app.js')
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+})
 
 // Compile SCSS files into CSS
 // ==========================================================================
 gulp.task('compileSass', function () {
-    return gulp.src("assets/styles/application.scss")
-        .pipe(customPlumber('Error compiling SCSS files :('))
-        .pipe(maps.init())
-        .pipe(sass({
-            // includes bower_components as a @import location
-            includePaths: ['./bower_components']
-        }))
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest('dist/assets/css'))
-        .pipe(bs.reload(
-            {stream: true}
-        ));
-});
-
+  return gulp.src('assets/styles/application.scss')
+    .pipe(customPlumber('Error compiling SCSS files :('))
+    .pipe(maps.init())
+    .pipe(sass({
+      // includes bower_components as a @import location
+      includePaths: ['./bower_components']
+    }))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(bs.reload(
+      { stream: true }
+    ))
+})
 
 // Watch for and compile on changes
 // ==========================================================================
-gulp.task('watchFiles', ["compileSass"], function () {
-    gulp.watch('assets/js/main.js', ['concatScripts']);
-    gulp.watch('assets/images/*', ['images']);
-    gulp.watch('assets/video/*', ['video']);
-    gulp.watch('assets/fonts/*', ['fonts']);
-    gulp.watch('assets/styles/**/*.scss', ['compileSass']);
-    gulp.watch('./*.html').on('change', bs.reload);
-});
+gulp.task('watchFiles', ['compileSass'], function () {
+  gulp.watch(['assets/js/*.js', '!assets/js/app.js'], ['concatScripts']).on('change', bs.reload)
+  gulp.watch('assets/images/*', ['images'])
+  gulp.watch('assets/video/*', ['video'])
+  gulp.watch('assets/fonts/*', ['fonts'])
+  gulp.watch('assets/styles/**/*.scss', ['compileSass'])
+  gulp.watch('./*.html').on('change', bs.reload)
+})
 
 // Compile and minify build files
 // ==========================================================================
-gulp.task("build", ['minifyScripts', 'compileSass', 'images', 'video', 'fonts'], function () {
-    return gulp.src(["assets/css/application.css", "assets/js/app.min.js",
-        "img/**", "fonts/**"], {base: './'})
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('build', ['minifyScripts', 'compileSass', 'images', 'video', 'fonts'], function () {
+  return gulp.src(['assets/css/application.css', 'assets/js/app.min.js',
+    'img/**', 'fonts/**'], { base: './' })
+    .pipe(gulp.dest('dist'))
+})
 
 // Install bower dependencies
 // ==========================================================================
 gulp.task('bower-install', function () {
-    return bower()
-        .pipe(notify({
-            title: 'Install complete',
-            message: 'Please run "gulp atomic", "gulp regular" or "gulp cssgrid" to finish setup.',
-            onLast: true
-        }));
-});
+  return bower()
+    .pipe(notify({
+      title: 'Install complete',
+      message: 'Please run "gulp atomic", "gulp regular" or "gulp cssgrid" to finish setup.',
+      onLast: true
+    }))
+})
 
 // Setup the project
 // ==========================================================================
 gulp.task('setup', ['clean', 'bower-install'], function () {
-});
+})
 
 // Select style type
 // ==========================================================================
 gulp.task('atomic', ['build'], function () {
-    return gulp.src("src/atomic/**/*")
-        .pipe(gulp.dest('assets/styles'))
-        .pipe(notify({
-            title: 'Install complete',
-            message: 'Atomic design system has been installed. You can now start the project with "gulp watch". Enjoy!',
-            onLast: true
-        }));
-});
+  return gulp.src('src/atomic/**/*')
+    .pipe(gulp.dest('assets/styles'))
+    .pipe(notify({
+      title: 'Install complete',
+      message: 'Atomic design system has been installed. You can now start the project with "gulp watch". Enjoy!',
+      onLast: true
+    }))
+})
 
 gulp.task('regular', ['build'], function () {
-    return gulp.src("src/regular/**/*")
-        .pipe(gulp.dest('assets/styles'))
-        .pipe(notify({
-            title: 'Install complete',
-            message: 'Default styles has been installed. You can now start the project with "gulp watch". Enjoy!',
-            onLast: true
-        }));
-});
+  return gulp.src('src/regular/**/*')
+    .pipe(gulp.dest('assets/styles'))
+    .pipe(notify({
+      title: 'Install complete',
+      message: 'Default styles has been installed. You can now start the project with "gulp watch". Enjoy!',
+      onLast: true
+    }))
+})
 
 gulp.task('grid-styles', ['build'], function () {
-    return gulp.src(['src/cssgrid/**/*', '!src/cssgrid/index.html'])
-        .pipe(gulp.dest('assets/styles'))
-        .pipe(notify({
-            title: 'Install complete',
-            message: 'CSS GRID styles has been installed. You can now start the project with "gulp watch". Enjoy!',
-            onLast: true
-        }));
-});
+  return gulp.src(['src/cssgrid/**/*', '!src/cssgrid/index.html'])
+    .pipe(gulp.dest('assets/styles'))
+    .pipe(notify({
+      title: 'Install complete',
+      message: 'CSS GRID styles has been installed. You can now start the project with "gulp watch". Enjoy!',
+      onLast: true
+    }))
+})
 
 gulp.task('grid-html', function () {
-    return gulp.src("src/cssgrid/index.html")
-        .pipe(gulp.dest('./'))
-});
+  return gulp.src('src/cssgrid/index.html')
+    .pipe(gulp.dest('./'))
+})
 
 gulp.task('php', function () {
-    del(['index.html']);
-    return gulp.src("src/php/**/*")
-        .pipe(gulp.dest('./'))
-        .pipe(notify({
-            title: 'Switched to PHP mode',
-            message: 'PHP dependencies has been installed. You can now start the project with "gulp watch". Enjoy!',
-            onLast: true
-        }));
-});
+  del(['index.html'])
+  return gulp.src('src/php/**/*')
+    .pipe(gulp.dest('./'))
+    .pipe(notify({
+      title: 'Switched to PHP mode',
+      message: 'PHP dependencies has been installed. You can now start the project with "gulp watch". Enjoy!',
+      onLast: true
+    }))
+})
 
-gulp.task('grid', ['grid-styles', 'grid-html']);
+gulp.task('grid', ['grid-styles', 'grid-html'])
 
 // Connect Browser Sync to watch command
 // ==========================================================================
-gulp.task('watch', ['browser-sync', 'watchFiles']);
+gulp.task('watch', ['browser-sync', 'watchFiles'])
 
 // Run clean task if just gulp (without any other parameter) is used
 // ==========================================================================
-gulp.task("default", ["clean"], function () {
-    gulp.start('build');
-});
+gulp.task('default', ['clean'], function () {
+  gulp.start('build')
+})
 
 // ==========================================================================
 // Clean stuff
@@ -216,17 +215,17 @@ gulp.task("default", ["clean"], function () {
 // Deletes the build folder completely
 // ==========================================================================
 gulp.task('clean', function () {
-    del(['dist', 'dist/css/application.css*', 'dist/js/app*.js*']);
-});
+  del(['dist', 'dist/css/application.css*', 'dist/js/app*.js*'])
+})
 
 // Deletes the styles folder within assets completely
 // ==========================================================================
 gulp.task('clean-styles', function () {
-    del(['assets/styles']);
-});
+  del(['assets/styles'])
+})
 
 // Deletes the src folder
 // ==========================================================================
 gulp.task('clean-src', function () {
-    del(['assets/styles']);
-});
+  del(['assets/styles'])
+})
